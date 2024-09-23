4f04329fe0b20eab2af2cdb35861d81cc86613b6
nishiiko <142626254+nishiiko@users.noreply.github.com>
2024-09-23 20:20:53 +0100

    Add files via upload

--- .config/bspwm/resources/scripts/screenshot_region.sh
+++ .config/bspwm/resources/scripts/screenshot_region.sh
@@ -3,3 +3,4 @@ name=$(date +"%Y-%m-%d_%H-%M")
 file_name=$name.png
 scrot -s -f ~/Pictures/Screenshots/$file_name
 xclip -selection clipboard -target image/png -i ~/Pictures/Screenshots/$file_name
+rm ~/Pictures/Screenshots/$file_name
